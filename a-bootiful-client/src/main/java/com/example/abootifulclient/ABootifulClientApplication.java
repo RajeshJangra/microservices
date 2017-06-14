b337b0320f15b3764f7215ef94e06b09a50f8270
Rajesh Kumar <rkumar@beachbody.com>
2017-06-14 18:47:43 +0530

    Changes

---
+++ a-bootiful-client/src/main/java/com/example/abootifulclient/ABootifulClientApplication.java
@@ -0,0 +1,12 @@
+package com.example.abootifulclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ABootifulClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ABootifulClientApplication.class, args);
+	}
+}
--- config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -1,12 +1,26 @@
 package com.example.configserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@EnableConfigServer
 @SpringBootApplication
+@RestController
 public class ConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-	}
+    @Value("${config.name ?: Rajesh}")
+    String name = "World";
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello " + name;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
 }
